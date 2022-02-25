c8f57302a8dfce9f7172b214d6765fcfb39dae31
risswazowlsky <85448152+risswazowlsky@users.noreply.github.com>
2022-02-26 00:09:14 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ async def is_admins(chat_id: int):
     ]
 
 levellink =["https://telegra.ph/file/6620fe683ff3989268c7f.mp4", "https://telegra.ph/file/c6bbce91cb75d4ab318ae.mp4", "https://telegra.ph/file/c2ac7b63d248f49da952c.mp4", "https://telegra.ph/file/b100466a5f0c42fa7255f.mp4", "https://telegra.ph/file/67c9dc7b59f78aa7aaf4c.mp4", "https://telegra.ph/file/06e2d74343e89c9d3cd12.mp4", "https://telegra.ph/file/88458a18eea8e86292b14.mp4", "https://telegra.ph/file/e3786d4f321ff4335a70f.mp4"]
-levelname = ["Team Rocket", "Stray God", "Vector", "Hero Association", "Z Warrior", "Black Knight", "Ghoul", "Overlord"]
+levelname = ["C", "A", "A+", "S", "S+", "SS+", "Lord", "Gods"]
 levelnum = [2,5,15,25,35,50,70,100]
 
 
@@ -64,15 +64,15 @@ async def levelsystem(_, message):
             await is_admins(chat_id)
         ):
             return await message.reply_text(
-                "You are not admin"
+                "Kamu bukan admin!"
             )
     is_level = toggle.find_one({"chat_id": message.chat.id})
     if not is_level:
         toggle.insert_one({"chat_id": message.chat.id})
-        await message.reply_text("Level System Enable")
+        await message.reply_text("✅ Level Sistem Enable")
     else:
         toggle.delete_one({"chat_id": message.chat.id})
-        await message.reply_text("Level System Disable")
+        await message.reply_text("❎ Level System Disable")
 
 
 @bot.on_message(
@@ -114,12 +114,12 @@ async def level(client, message):
                     l += 1
                 xp -= ((50*((l-1)**2))+(50*(l-1)))
                 if xp == 0:
-                    await message.reply_text(f"🌟 {message.from_user.mention}, You have reached level {l}**, Nothing can stop you on your way!")
+                    await message.reply_text(f"⚡ {message.from_user.mention}, Kamu telah mencapai batas! {l}**, Tidak ada yang bisa menghentikan mu!")
     
                     for lv in range(len(levelname)) and range(len(levellink)):
                             if l == levelnum[lv]:            
                                 Link = f"{levellink[lv]}"
-                                await message.reply_video(video=Link, caption=f"{message.from_user.mention}, You have reached Rank Name **{levelname[lv]}**")
+                                await message.reply_video(video=Link, caption=f"{message.from_user.mention}, Rank Kamu Sudah Dicapai **{levelname[lv]}**")
                   
 
                                
